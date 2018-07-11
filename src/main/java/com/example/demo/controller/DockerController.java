8b7f8dddcf1aa9b73936e20888afc46fbe5804dc
Ramjean <zafiirah.ramjean@accenture.com>
2018-07-11 14:19:03 +0400

    firstcommit

---
+++ src/main/java/com/example/demo/controller/DockerController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class DockerController {
+	
+	
+	@GetMapping("/")
+	public String hello() {
+	return "hello";
+	}
+
+	@PostMapping("/user")
+	public String helloSomeone(@RequestParam String user, Model model) {
+	model.addAttribute("user", user);
+	return "hello";
+	}
+	
+}
